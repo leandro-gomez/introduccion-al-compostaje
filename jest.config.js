1eae1e2c2d251b9d02af35947be6d374e3bc43e7
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-09-19 14:47:53 -0500

    Use jest instead

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "@vue/cli-plugin-unit-jest",
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,vue}", "!src/main.js", "!src/App.vue"],
+};
