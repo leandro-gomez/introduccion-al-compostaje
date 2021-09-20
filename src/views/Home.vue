0b6a45e4b3506757afa3e4ff3e4a8db61fe244a1
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-09-20 17:13:58 -0500

    Add buttons to game options

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,59 @@
 <template>
-  <div class="home">
-    <h1>Introducción al Compostaje mediante el juego</h1>
-    <p>
-      Para referencias detalladas, consulte los siguientes documentos:
-    </p>
-    <ul class="list-unstyled">
-      <li>
-        <a
-          href="https://www.inti.gob.ar/publicaciones/descargac/348"
-          target="_blank"
-          rel="noopener"
-        ><i class="fas fa-hand-point-right" /> Manual de buenas practicas para producir compost hogareño</a>
-      </li>
-      <li>
-        <a
-          href="https://www.argentina.gob.ar/sites/default/files/instructivo_para_la_produccion_de_compost_domiciliario.pdf"
-          target="_blank"
-          rel="noopener"
-        ><i class="fas fa-hand-point-right" /> Instructivo para la produccion de Compost Domiciliario</a>
-      </li>
-    </ul>
+  <div class="row">
+    <div class="col">
+      <h1>Introducción al Compostaje mediante el juego</h1>
+      <p>
+        Para referencias detalladas, consulte los siguientes documentos:
+      </p>
+      <ul class="list-unstyled">
+        <li>
+          <a
+            href="https://www.inti.gob.ar/publicaciones/descargac/348"
+            target="_blank"
+            rel="noopener"
+          >
+            <i class="fas fa-hand-point-right" /> Manual de buenas prácticas
+            para producir compost hogareño
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.argentina.gob.ar/sites/default/files/instructivo_para_la_produccion_de_compost_domiciliario.pdf"
+            target="_blank"
+            rel="noopener"
+          >
+            <i class="fas fa-hand-point-right" /> Instructivo para la producción
+            de Compost Domiciliario
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <h3>O prueba nuestro juego</h3>
+      <br/>
+      <div
+        class="btn-group"
+        role="group"
+        aria-label="Empezar"
+      >
+        <button
+          type="button"
+          class="btn btn-success"
+        >
+          ¿Cómo crear un Compost?
+        </button>
+        <button
+          type="button"
+          class="btn btn-success"
+        >
+          ¿Cómo usar un Compost?
+        </button>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-
-
-export default {
-  name: 'Home',
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
